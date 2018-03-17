60f9af4cd845c0f19691ea7ae4f49bd2db291206
dafuyi <1427019390@qq.com>
2018-03-17 13:10:42 +0800

    a header file for opengl

---
+++ src/include/base.h
@@ -0,0 +1,14 @@
+/**
+ * my opengl include file
+ * include all *.h for a opengl program
+ * @author yidafu
+ * @time 2018-04-17
+ */
+#include <GL/glut.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glx.h>
+#include <GL/gl.h>
+#include <X11/X.h>
+#include <X11/keysym.h>
+#include <math.h>
