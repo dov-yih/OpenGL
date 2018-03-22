7337f4af3339b00330668303be775352b160c2c6
dafuyi <1427019390@qq.com>
2018-03-22 14:31:00 +0800

    add src file for homeword

---
+++ src/line.cpp
@@ -0,0 +1,15 @@
+/**
+ * --- src/line.cpp
+ * first opengl program.
+ * how install opengl on arch/manjaro  @ref https://en.wikibooks.org/wiki/OpenGL_Programming/Installation/Linux
+ * @author yidafu
+ * @time 2018-4/17
+ */
+
+#include "include/base.h"
+
+int main() {
+
+
+	return 0;
+}
